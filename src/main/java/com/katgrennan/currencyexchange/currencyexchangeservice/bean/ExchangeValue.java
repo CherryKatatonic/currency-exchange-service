29b541aaed85aba9e495a040bca5c115496ff158
nunya biz <nunya@bizniz.con>
2020-09-14 22:42:25 -0400

    Specify column names for 'from' & 'to' to avoid SQL keyword conflict

--- src/main/java/com/katgrennan/currencyexchange/currencyexchangeservice/bean/ExchangeValue.java
+++ src/main/java/com/katgrennan/currencyexchange/currencyexchangeservice/bean/ExchangeValue.java
@@ -1,5 +1,6 @@
 package com.katgrennan.currencyexchange.currencyexchangeservice.bean;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
@@ -9,7 +10,9 @@ public class ExchangeValue {
 
     @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private int port;
