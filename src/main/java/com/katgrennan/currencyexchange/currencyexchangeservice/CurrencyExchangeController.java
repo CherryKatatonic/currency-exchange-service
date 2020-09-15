d06a6a943db51692b591b34db33ad5020ec01be7
nunya biz <nunya@bizniz.con>
2020-09-14 22:05:43 -0400

    Add hard-coded getExchangeValue method

---
+++ src/main/java/com/katgrennan/currencyexchange/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,25 @@
+package com.katgrennan.currencyexchange.currencyexchangeservice;
+
+import com.katgrennan.currencyexchange.currencyexchangeservice.bean.ExchangeValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue getExchangeValue(@PathVariable String from, @PathVariable String to) {
+        ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+        exchangeValue.setPort(Integer.parseInt(env.getProperty("server.port")));
+        return exchangeValue;
+    }
+
+}
