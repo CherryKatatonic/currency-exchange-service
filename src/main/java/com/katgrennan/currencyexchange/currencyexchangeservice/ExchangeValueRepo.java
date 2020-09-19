a673762a5074ebf7ff0652e9833c51c014bde1ce
nunya biz <nunya@bizniz.con>
2020-09-18 21:31:58 -0400

    Add ExchangeValueRepository

---
+++ src/main/java/com/katgrennan/currencyexchange/currencyexchangeservice/ExchangeValueRepo.java
@@ -0,0 +1,9 @@
+package com.katgrennan.currencyexchange.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepo extends JpaRepository<ExchangeValue, Long> {
+
+    ExchangeValue findByFromAndTo(String from, String to);
+
+}
