4cb4855f4d1856f17ad6d01e807418efc82584a8
nunya biz <nunya@bizniz.con>
2020-09-18 21:33:00 -0400

    Replace hard-coded ExchangeValue with result from repo

--- src/main/java/com/katgrennan/currencyexchange/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/katgrennan/currencyexchange/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,5 @@
 package com.katgrennan.currencyexchange.currencyexchangeservice;
 
-import com.katgrennan.currencyexchange.currencyexchangeservice.bean.ExchangeValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @RestController
 public class CurrencyExchangeController {
@@ -15,10 +15,13 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment env;
 
+    @Autowired
+    private ExchangeValueRepo repo;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue getExchangeValue(@PathVariable String from, @PathVariable String to) {
-        ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
-        exchangeValue.setPort(Integer.parseInt(env.getProperty("server.port")));
+        ExchangeValue exchangeValue = repo.findByFromAndTo(from, to);
+        exchangeValue.setPort(Integer.parseInt(Objects.requireNonNull(env.getProperty("server.port"))));
         return exchangeValue;
     }
 
