966d0f575dd606f7493ad41f7a6a0558a2834532
nunya biz <nunya@bizniz.con>
2020-09-20 21:45:27 -0400

    Add Eureka Client

--- src/main/java/com/katgrennan/currencyexchange/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/katgrennan/currencyexchange/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,8 +2,11 @@ package com.katgrennan.currencyexchange.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+// Enable discovery for Eureka naming server
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
